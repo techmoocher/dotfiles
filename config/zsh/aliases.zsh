129bfc009b94b8064ab0920df9957b0c0260098f
techmoocher <92527665+techmoocher@users.noreply.github.com>
2026-04-11 09:19:59 +0700

    feat: zsh

---
+++ config/zsh/aliases.zsh
@@ -0,0 +1,20 @@
+###### ALIASES ######
+
+alias la='ls -la --color=auto'
+alias ls='ls --color=auto'
+alias lah='ls -lah --color=auto'
+alias grep='grep --color=auto'
+alias rm='rm -i'
+
+alias ghome='cd "$HOME" ; echo "Welcome home! "'
+alias gconfig='cd "$HOME/.config" ; echo "Tinker time! "'
+alias gdown='cd "$HOME/Downloads" ; echo "Linh ta linh tinh!"'
+
+alias python='python3'
+
+alias yh='yazi "$HOME/"'
+alias yd='yazi "$HOME/Downloads/"'
+alias yc='yazi "$HOME/.config/"'
+alias yP='yazi "$HOME/\!\!\!PERSONAL\!\!\!/"'
+
+alias colors='fastfetch -s colors'
